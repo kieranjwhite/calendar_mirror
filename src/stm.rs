f1af63364e1b83022915b081c0678cd36f9cd982
Kieran White <kieran.white@hourglassapps.com>
2019-08-08 17:34:27 +0100

    now stm macro seems to work with initialising tuple of all arities up to and including 32

--- src/stm.rs
+++ src/stm.rs
@@ -436,7 +436,7 @@ macro_rules! stm {
                     } )*
                 )*;
 
-                crate::stm!(@append_tuple $start_trailing, arg, (0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15), ($($start_arg,)*) -> $enum_name::$start(node))
+                crate::stm!(@append_tuple $start_trailing, arg, (0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31), ($($start_arg,)*) -> $enum_name::$start(node))
             }
 
             #[allow(dead_code)]
